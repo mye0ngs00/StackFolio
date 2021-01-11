490929d2202b938cc3ee87e0e6fbfcfd6d5aa7ca
ehgks0000 <ehgks0083@gmail.com>
2021-01-11 16:34:57 +0900

    typeorm connection

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -7,9 +7,11 @@ import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { LikesModule } from './likes/likes.module';
 import { FollowersModule } from './followers/followers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './orm.config';
 
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule, CommentsModule, LikesModule, FollowersModule],
+  imports: [TypeOrmModule.forRoot(config),AuthModule, UsersModule, PostsModule, CommentsModule, LikesModule, FollowersModule],
   controllers: [AppController],
   providers: [AppService],
 })
