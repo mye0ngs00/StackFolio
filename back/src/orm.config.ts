5dfeb7ecd242f0e1094c858782d3ce9edbb328cc
ehgks0000 <ehgks0083@gmail.com>
2021-01-14 22:45:21 +0900

    ㅁㄴㅇㄻㄴㅇㄹ

--- back/src/orm.config.ts
+++ back/src/orm.config.ts
@@ -21,7 +21,7 @@ export const config: TypeOrmModuleOptions = inContainer
   : {
       type: 'postgres',
       username: 'postgres',
-      password: 'dhfpswl112',
+      password: 'root',
       port: 5432,
       host: '127.0.0.1',
       database: 'nest-typeorm',
