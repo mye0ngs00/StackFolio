e956610df57c711b7ca628ad68e1e1b8f639a959
David Park <dpjungmin@gmail.com>
2021-01-14 21:16:07 +0900

    Add secure layer for production mode

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,19 +1,35 @@
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 const isProd = process.env.NODE_ENV === 'production';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = +process.env.PORT || 3000;
+
+  app.useGlobalPipes(new ValidationPipe());
 
   if (isProd) {
+    app.use(helmet());
+    app.use(
+      rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+      }),
+    );
     // Starts listening for shutdown hooks
     // https://docs.nestjs.com/fundamentals/lifecycle-events#application-shutdown
     app.enableShutdownHooks();
   }
 
+  const port = +process.env.PORT || 3000;
+
   await app.listen(port);
-  console.log(`🚀 Server is running on: ${await app.getUrl()}`);
+
+  new Logger('main').verbose(
+    `🚀 Application is running on: ${await app.getUrl()}`,
+  );
 }
 bootstrap();
