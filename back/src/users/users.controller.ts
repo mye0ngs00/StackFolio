75e64785065a695c20fb9aff2b696cd56b429320
ehgks0000 <ehgks0083@gmail.com>
2021-01-16 22:24:16 +0900

    nodemailer 추가

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,14 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
+import { MailService } from 'src/mail/mail.service';
 import { User } from './entity/user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
-    constructor(private readonly userService: UsersService){}
+    constructor(private readonly userService: UsersService,
+        private readonly mailService: MailService){}
 
     @Get()
     findAll(): Promise<User[]> {
         return this.userService.findAll();
+    }
+
+    @Get("send-mail")
+    sendMail(): any {
+        try {
+            this.mailService.sendingMail();
+            return {message: "성공"};
+        } catch (err) {
+            return {message: "에러", err};
+        }
     }
 }
--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -4,9 +4,11 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './repository/UserRepository';
 import { UserProfileRepository } from './repository/UserProfileRepository';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository, UserProfileRepository])],
+    imports: [TypeOrmModule.forFeature([UserRepository, UserProfileRepository]),
+MailModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports:[UsersService],
